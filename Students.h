65da975a8f800cbf6d5d095236ea6f39f4640ef5
Stephen Wehlburg <swehlburg@ufl.edu>
2018-04-23 00:45:36 -0400

    Comments to Student.h

--- Students.h
+++ Students.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #ifndef STUDENTS_STUDENTS_H
 #define STUDENTS_STUDENTS_H
@@ -7,11 +6,13 @@
 #include <iostream>
 #include "Assignments.cpp"
 
+//Used for the LinkedList class for assignments
 struct Node{
     Assignments assignmentNode;
     Node *next;
 };
 
+//Class to keep track of assignments student has done
 class LinkedList{
 private:
     Node *head;
@@ -22,18 +23,25 @@ public:
     void removeAssignment(std::string assignmentName);
 };
 
-
+//The values and methods used by the students object
 class Students{
     private:
         std::string name;
         int ID;
     public:
+		//Constructors
         Students();
         Students(std::string name, int ID);
+
+		//Assignment linked list
         LinkedList assignments;
+
+		//Get methods
         std::string getName();
+		int getID();
+
+		//Set methods
         void setName(std::string name);
-        int getID();
         void setID(int ID);
 };
 
