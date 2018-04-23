7ba1da39c3575e409599298a67ec869a22d3b0ce
Stephen Wehlburg <swehlburg@ufl.edu>
2018-04-23 00:17:10 -0400

    Comments to Student.cpp

--- Students.cpp
+++ Students.cpp
@@ -1,37 +1,65 @@
 #include <iostream>
 #include "Students.h"
 
+/*
+Student Constructors
+*/
+
+//Required default constructor
 Students::Students(){
     this->name = "";
     this->ID = 0;
 };
 
+//The actually used constructor
 Students::Students(std::string name, int ID){
     this->name = name;
     this->ID = ID;
 };
 
+/*
+Get Methods
+*/
+
+//Returns the student ID
 int Students::getID() {
     return ID;
 };
 
+//Returns the student Name
 std::string Students::getName() {
     return name;
 };
 
+/*
+Set Methods
+*/
+
+//Changes the student ID to the given number
 void Students::setID(int ID) {
     this->ID = ID;
 };
 
+//Changes the student name to the given name
 void Students::setName(std::string name) {
     this->name = name;
 };
 
+/*
+Assignment Linked List Constructors
+*/
+
+//The initial default constructor
 LinkedList::LinkedList() {
     head = NULL;
     tail = NULL;
 };
 
+/*
+Assignment Management
+*/
+
+//Adds assignments to student
 void LinkedList::addAssignment(Assignments assignment) {
     Node *current = new Node;
     Node *temp = new Node;
@@ -56,6 +84,7 @@ void LinkedList::addAssignment(Assignments assignment) {
     tail = tail->next;
 };
 
+//Removes assignments from students
 void LinkedList::removeAssignment(std::string assignmentName) {
     Node *temp = new Node;
     temp = head;
@@ -89,4 +118,3 @@ void LinkedList::removeAssignment(std::string assignmentName) {
        temp3->next = temp->next;
    }
 };
-
